7b8aa248e712800e8d843f7b7c8d0c5aa7de54da
Wang <msj1013654754@gmail.com>
2022-11-13 22:40:54 +0800

    update code

--- app/src/main/java/github/zerorooot/copyfenbi/Xposed.kt
+++ app/src/main/java/github/zerorooot/copyfenbi/Xposed.kt
@@ -1,5 +1,6 @@
 package github.zerorooot.copyfenbi
 
+import android.R.attr.classLoader
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -12,35 +13,47 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 class Xposed : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: LoadPackageParam) {
         if (lpparam.packageName.equals("com.fenbi.android.servant")) {
-            val episodeCommentClass =
-                XposedHelpers.findClass("com.fenbi.android.ke.data.EpisodeComment", lpparam.classLoader)
-            val ky = XposedHelpers.findClass("ky0\$a", lpparam.classLoader)
-            XposedHelpers.findAndHookMethod(
-                "dc4",
+            XposedHelpers.findAndHookMethod("com.netease.nis.wrapper.MyApplication",
                 lpparam.classLoader,
-                "r",
-                episodeCommentClass,
-                View::class.java,
-                Int::class.javaPrimitiveType,
-                ky,
+                "a",
+                Context::class.java,
                 object : XC_MethodHook() {
                     @Throws(Throwable::class)
                     override fun beforeHookedMethod(param: MethodHookParam) {
-                        val context = (param.args[1] as View).context
-                        val nickName = XposedHelpers.callMethod(param.args[0], "getNickName") as String
-                        val comment = XposedHelpers.callMethod(param.args[0], "getComment") as String
-
-                        val clipboard =
-                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                        val clip: ClipData = ClipData.newPlainText("copy fen bi", comment)
-                        clipboard.setPrimaryClip(clip)
-
-                        Toast.makeText(context, "用户 $nickName 的评论已复制到剪贴板", Toast.LENGTH_SHORT).show()
-                        param.result = false
+                        fenbi((param.args[0] as Context).classLoader)
                     }
-                }
-            )
+                })
         }
 
     }
+
+    private fun fenbi(classLoader: ClassLoader) {
+        val episodeCommentClass =
+            XposedHelpers.findClass("com.fenbi.android.ke.data.EpisodeComment", classLoader)
+        val ky = XposedHelpers.findClass("ky0\$a", classLoader)
+        XposedHelpers.findAndHookMethod("dc4",
+            classLoader,
+            "r",
+            episodeCommentClass,
+            View::class.java,
+            Int::class.javaPrimitiveType,
+            ky,
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val context = (param.args[1] as View).context
+                    val nickName = XposedHelpers.callMethod(param.args[0], "getNickName") as String
+                    val comment = XposedHelpers.callMethod(param.args[0], "getComment") as String
+
+                    val clipboard =
+                        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val clip: ClipData = ClipData.newPlainText("copy fen bi", comment)
+                    clipboard.setPrimaryClip(clip)
+
+                    Toast.makeText(context, "用户 $nickName 的评论已复制到剪贴板", Toast.LENGTH_SHORT).show()
+                    param.result = false
+                }
+            })
+    }
+
 }
