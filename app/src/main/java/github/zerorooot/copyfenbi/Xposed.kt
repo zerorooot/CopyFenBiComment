758a2129c6578bc3c7dce64c56f0c94ede6e3cf1
Wang <msj1013654754@gmail.com>
2023-01-17 15:01:02 +0800

    新增隐藏讲解视频功能

--- app/src/main/java/github/zerorooot/copyfenbi/Xposed.kt
+++ app/src/main/java/github/zerorooot/copyfenbi/Xposed.kt
@@ -5,6 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.os.Environment
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
 import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
@@ -12,6 +13,37 @@ import java.io.File
 
 
 class Xposed : IXposedHookLoadPackage {
+    private val replaceWord: Array<String> by lazy {
+        arrayOf(
+            "老师讲的真好",
+            "清晰明了",
+            "很棒棒",
+            "满分！",
+            "\uD83D\uDC4D\uD83D\uDC4D\uD83D\uDC4D\uD83D\uDC4D",
+            "懂了",
+            "拓展的很好",
+            "老师讲的挺好 咬字清晰 语速适当 爱了 爱了",
+            "下次还错-.-",
+            "吃了没文化的亏",
+            "基础不牢，地动山摇",
+            "不拖泥带水 很好",
+            "赞！",
+            "讲的很清晰明了，可以加鸡腿了",
+            "满分",
+            "负责的老师",
+            "老喜欢这个老师了！讲的真好",
+            "讲解得非常全面",
+            "讲的很好，学到了一些不知道的知识",
+            "老师讲的很好！！",
+            "我对了,我蒙的",
+            "每次最痛苦的就是二选一，最最痛苦的就是每次二选一都选错ᇂ_ᇂ",
+            "真好，拓展了很多知识",
+            "眼瞎的集合",
+            "不错！全讲到了！棒棒棒！",
+            "好"
+        )
+    }
+
     override fun handleLoadPackage(lpparam: LoadPackageParam) {
         if (lpparam.packageName.equals("com.fenbi.android.servant")) {
             XposedHelpers.findAndHookMethod("com.netease.nis.wrapper.MyApplication",
@@ -23,10 +55,13 @@ class Xposed : IXposedHookLoadPackage {
                     override fun beforeHookedMethod(param: MethodHookParam) {
                         val classLoader = (param.args[0] as Context).classLoader
                         copy(classLoader)
-                        val config =
-                            Environment.getExternalStorageDirectory().toString() + "/Android/dz"
-                        if (File(config).exists()) {
+
+                        if (check("dz")) {
                             filtration(classLoader)
+                        }
+
+                        if (check("sp")) {
+                            hideVideo(classLoader)
                         }
 
                     }
@@ -75,39 +110,45 @@ class Xposed : IXposedHookLoadPackage {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     val comment = XposedHelpers.callMethod(param.args[0], "getComment") as String
                     if (comment.contains("上岸") || comment.contains("点赞")) {
-                        val replaceWord = arrayOf(
-                            "老师讲的真好",
-                            "清晰明了",
-                            "很棒棒",
-                            "满分！",
-                            "\uD83D\uDC4D\uD83D\uDC4D\uD83D\uDC4D\uD83D\uDC4D",
-                            "懂了",
-                            "拓展的很好",
-                            "老师讲的挺好 咬字清晰 语速适当 爱了 爱了",
-                            "下次还错-.-",
-                            "吃了没文化的亏",
-                            "基础不牢，地动山摇",
-                            "不拖泥带水 很好",
-                            "赞！",
-                            "讲的很清晰明了，可以加鸡腿了",
-                            "满分",
-                            "负责的老师",
-                            "老喜欢这个老师了！讲的真好",
-                            "讲解得非常全面",
-                            "讲的很好，学到了一些不知道的知识",
-                            "老师讲的很好！！",
-                            "我对了,我蒙的",
-                            "每次最痛苦的就是二选一，最最痛苦的就是每次二选一都选错ᇂ_ᇂ",
-                            "真好，拓展了很多知识",
-                            "眼瞎的集合",
-                            "不错！全讲到了！棒棒棒！",
-                            "好"
-                        )
                         val replace = replaceWord.random()
                         XposedBridge.log("CopyFenbi $comment -> $replace")
                         XposedHelpers.setObjectField(param.args[0], "comment", replace)
                     }
                 }
             })
+    }
+
+    private fun hideVideo(classLoader: ClassLoader) {
+        val episode =
+            XposedHelpers.findClass("com.fenbi.android.business.ke.data.Episode", classLoader)
+        val userMemberState = XposedHelpers.findClass(
+            "com.fenbi.android.business.vip.data.UserMemberState",
+            classLoader
+        )
+        XposedHelpers.findAndHookMethod("com.fenbi.android.question.common.render.MemberVideoRender",
+            classLoader,
+            "Q",
+            String::class.java,
+            episode,
+            userMemberState,
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val linearLayout = (XposedHelpers.getObjectField(
+                        param.thisObject,
+                        "i"
+                    ) as LinearLayout)
+
+                    //R$id.member_video_wrapper
+                    linearLayout.findViewById<View>(2131365094).visibility = View.GONE
+
+                }
+            })
+    }
+
+    private fun check(path: String): Boolean {
+        val config =
+            Environment.getExternalStorageDirectory().toString() + "/Android/" + path
+        return File(config).exists()
     }
 }
